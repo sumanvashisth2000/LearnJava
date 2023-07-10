338ed7a1ffe59e169eb338de327c1a1bcffee38c
sumanvashisth2000 <45479082+sumanvashisth2000@users.noreply.github.com>
2023-07-10 08:02:48 +0530

    Create First Commit Program001

---
+++ Chapter01/Program001.java
@@ -0,0 +1,8 @@
+//Program 1 using Java
+public class Program001{
+    public static void main(String [] args){
+        System.out.println("Hello World ");
+        System.out.println("Hello Java Learner, This is Program 1 ");
+    
+    }
+}
