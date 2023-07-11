f10320875e7fd7ea6cecaa466e171dbf12cd354a
sumanvashisth2000 <45479082+sumanvashisth2000@users.noreply.github.com>
2023-07-11 15:39:47 +0530

    First Commit Program 2

---
+++ Chapter01/Program002.java
@@ -0,0 +1,8 @@
+//Program 2 using Java
+public class Program002{
+    public static void main(String [] args){
+        System.out.println("Hello World ");
+        System.out.println("Hello Java Learner, This is Program 2 ");
+    
+    }
+}
